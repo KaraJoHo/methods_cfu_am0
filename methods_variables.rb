1718fee643c5d9b1577f55cdbab581ce5e1856fe
Kara <karajones-hofmann@Karas-MacBook-Pro.local>
2022-08-30 10:42:13 -0600

    This commit includes part 2 work

--- methods_variables.rb
+++ methods_variables.rb
@@ -28,9 +28,27 @@ puts my_name.length
 user_name = "coco_11am"
 last_login = "12/09/2021"
 
+puts user_name.delete_prefix("user")
+
+#delete_prefix method will take the argument passed to it
+#and delete the prefix of the string object
+
+puts user_name.replace("named_user")
+# the replace method will replace the contents of the string object
+#with the contents of the argument passed to it.
+
+puts user_name.concat ("Is", "Name")
+# the concat method links the arguments passed to the
+#string object and returns
+
+puts user_name.delete_prefix! ("name")
+# the delete prefix! will return nil if the argument passed to it
+#is not the leading prefix in the string object
+
 # 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do.
   # Write the methods out on the lines below, with an explanation in your own words of how they work.
 
 
 
 # 2: Do some research (either testing out with your own code or Googling) to build an understanding of what the `!` does, when at the end of a method name. Show your understading by providing an example and writing an explanation.
+# ! can also be called a bang and it modified the code permanently
